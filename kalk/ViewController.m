ed869d8d9e659365275bcc31489ab3441b1952a3
usernamenameuser <writeme@email.ua>
2017-02-28 02:04:37 +0200

    kalk

--- kalk/ViewController.m
+++ kalk/ViewController.m
@@ -10,9 +10,50 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *displayLabel;
+@property (weak, nonatomic) IBOutlet UILabel *displayLabel1;
+
+
 @end
 
 @implementation ViewController
+
+bool flagg;
+
+- (IBAction)cifraBtn:(UIButton *)sender {
+    NSString *btn = sender.currentTitle;
+    NSString *disp = _displayLabel.text;
+    if (flagg) {
+        _displayLabel.text = [disp stringByAppendingString:btn];
+    }
+    else {
+        _displayLabel.text = btn;
+        flagg = true;
+    }
+}
+
+
+- (IBAction)posch:(UIButton *)sender {
+    int a = 0;
+    for (int i = 0; i < 3; ++i) {
+        NSString *ch = [NSString stringWithFormat: @"%c", [_displayLabel.text characterAtIndex:i]];
+        int ppp = [ch intValue];
+        a = a + ppp;
+    }
+    
+    int b = 0;
+    for (int i = 3; i < 6; ++i) {
+        NSString *ch = [NSString stringWithFormat: @"%c", [_displayLabel.text characterAtIndex:i]];
+        int ppp = [ch intValue];
+        b = b + ppp;
+    }
+    if (a == b) {
+        _displayLabel1.text = [NSString stringWithFormat:@"Билетик счастливый! Приятного аппетита =)"];
+    }
+    else {
+        _displayLabel1.text = [NSString stringWithFormat:@"Спробуйте ще =("];
+    }
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
